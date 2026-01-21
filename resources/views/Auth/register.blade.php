b99669e3e846bfce7dcefd0845e8006e785e7111
Kkiiiiiii <rikizhakaria@gmail.com>
2026-01-21 11:28:08 +0700

    register

--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -12,25 +12,25 @@ License: https://keenthemes.com/metronic/tailwind/docs/getting-started/license
 <html class="h-full" data-kt-theme="true" data-kt-theme-mode="light" dir="ltr" lang="en">
  <head><base href="../../../../../">
   <title>
-   Metronic - Tailwind CSS Sign In
+   Metronic - Tailwind CSS Sign Up
   </title>
   <meta charset="utf-8"/>
   <meta content="follow, index" name="robots"/>
-  <link href="https://127.0.0.1:8001/metronic-tailwind-html/demo4/authentication/classic/sign-in/index.html" rel="canonical"/>
+  <link href="https://127.0.0.1:8001/metronic-tailwind-html/demo4/authentication/classic/sign-up/index.html" rel="canonical"/>
   <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport"/>
-  <meta content="Sign in page using Tailwind CSS" name="description"/>
+  <meta content="Sign up page, powered by Tailwind CSS" name="description"/>
   <meta content="@keenthemes" name="twitter:site"/>
   <meta content="@keenthemes" name="twitter:creator"/>
   <meta content="summary_large_image" name="twitter:card"/>
-  <meta content="Metronic - Tailwind CSS Sign In" name="twitter:title"/>
-  <meta content="Sign in page using Tailwind CSS" name="twitter:description"/>
+  <meta content="Metronic - Tailwind CSS Sign Up" name="twitter:title"/>
+  <meta content="Sign up page, powered by Tailwind CSS" name="twitter:description"/>
   <meta content="assets/media/app/og-image.png" name="twitter:image"/>
-  <meta content="https://127.0.0.1:8001/metronic-tailwind-html/demo4/authentication/classic/sign-in/index.html" property="og:url"/>
+  <meta content="https://127.0.0.1:8001/metronic-tailwind-html/demo4/authentication/classic/sign-up/index.html" property="og:url"/>
   <meta content="en_US" property="og:locale"/>
   <meta content="website" property="og:type"/>
   <meta content="@keenthemes" property="og:site_name"/>
-  <meta content="Metronic - Tailwind CSS Sign In" property="og:title"/>
-  <meta content="Sign in page using Tailwind CSS" property="og:description"/>
+  <meta content="Metronic - Tailwind CSS Sign Up" property="og:title"/>
+  <meta content="Sign up page, powered by Tailwind CSS" property="og:description"/>
   <meta content="assets/media/app/og-image.png" property="og:image"/>
   <link href="assets/media/app/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180"/>
   <link href="assets/media/app/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png"/>
@@ -80,37 +80,52 @@ License: https://keenthemes.com/metronic/tailwind/docs/getting-started/license
   </style>
   <div class="flex items-center justify-center grow bg-center bg-no-repeat page-bg">
    <div class="kt-card max-w-[370px] w-full">
-    <form action="#" class="kt-card-content flex flex-col gap-5 p-10" id="sign_in_form" method="get">
+    <form action="#" class="kt-card-content flex flex-col gap-5 p-10" id="sign_up_form" method="post">
      <div class="text-center mb-2.5">
       <h3 class="text-lg font-medium text-mono leading-none mb-2.5">
-       Sign in
+       Sign up
       </h3>
-      <div class="flex items-center justify-center font-medium">
+      <div class="flex items-center justify-center">
        <span class="text-sm text-secondary-foreground me-1.5">
-        Need an account?
+        Already have an Account ?
        </span>
-       <a class="text-sm link" href="html/demo4/authentication/classic/sign-up.html">
-        Sign up
+       <a class="text-sm link" href="html/demo4/authentication/classic/sign-in.html">
+        Sign In
        </a>
+      </div>
+     </div>
+     <div class="flex items-center gap-2">
+     <div class="flex flex-col gap-1">
+      <label class="kt-form-label text-mono">
+       Email
+      </label>
+      <input class="kt-input" name="user_email" placeholder="email@email.com" type="text" value=""/>
+     </div>
+     <div class="flex flex-col gap-1">
+      <label class="kt-form-label font-normal text-mono">
+       Password
+      </label>
+      <div class="kt-input" data-kt-toggle-password="true">
+       <input name="user_password" placeholder="Enter Password" type="password" value="">
+        <button class="kt-btn kt-btn-sm kt-btn-ghost kt-btn-icon bg-transparent! -me-1.5" data-kt-toggle-password-trigger="true" type="button">
+         <span class="kt-toggle-password-active:hidden">
+          <i class="ki-filled ki-eye text-muted-foreground">
+          </i>
+         </span>
+         <span class="hidden kt-toggle-password-active:block">
+          <i class="ki-filled ki-eye-slash text-muted-foreground">
+          </i>
+         </span>
+        </button>
+       </input>
       </div>
      </div>
      <div class="flex flex-col gap-1">
       <label class="kt-form-label font-normal text-mono">
-       Email
+       Confirm Password
       </label>
-      <input class="kt-input" placeholder="email@email.com" type="text" value=""/>
-     </div>
-     <div class="flex flex-col gap-1">
-      <div class="flex items-center justify-between gap-1">
-       <label class="kt-form-label font-normal text-mono">
-        Password
-       </label>
-       <a class="text-sm kt-link shrink-0" href="html/demo4/authentication/classic/reset-password/enter-email.html">
-        Forgot Password?
-       </a>
-      </div>
       <div class="kt-input" data-kt-toggle-password="true">
-       <input name="user_password" placeholder="Enter Password" type="password" value=""/>
+       <input name="user_password" placeholder="Re-enter Password" type="password" value=""/>
        <button class="kt-btn kt-btn-sm kt-btn-ghost kt-btn-icon bg-transparent! -me-1.5" data-kt-toggle-password-trigger="true" type="button">
         <span class="kt-toggle-password-active:hidden">
          <i class="ki-filled ki-eye text-muted-foreground">
@@ -123,14 +138,17 @@ License: https://keenthemes.com/metronic/tailwind/docs/getting-started/license
        </button>
       </div>
      </div>
-     <label class="kt-label">
+     <label class="kt-checkbox-group">
       <input class="kt-checkbox kt-checkbox-sm" name="check" type="checkbox" value="1"/>
       <span class="kt-checkbox-label">
-       Remember me
+       I accept
+       <a class="text-sm link" href="#">
+        Terms & Conditions
+       </a>
       </span>
      </label>
      <button class="kt-btn kt-btn-primary flex justify-center grow">
-      Sign In
+      Sign up
      </button>
     </form>
    </div>
