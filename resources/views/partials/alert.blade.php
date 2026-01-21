9a8373fbd37642c8c7ccbd5ddc623d3fc1fdf489
Kkiiiiiii <rikizhakaria@gmail.com>
2026-01-21 16:21:02 +0700

    alert error

--- resources/views/partials/alert.blade.php
+++ resources/views/partials/alert.blade.php
@@ -14,3 +14,10 @@
         <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
     </div>
 @endif
+
+@if (session('error'))
+    <div class="alert alert-danger alert-dismissible fade show">
+        {{ session('error') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    </div>
+@endif
